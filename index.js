19573f542d1004528d7d81b97f96212acdc32168
FrederikDiamonD <frederikhandberg02@gmail.com>
2023-01-01 01:58:00 +0100

    Well... The data is working now.

--- index.js
+++ index.js
@@ -61,6 +61,7 @@ mongoose.connect(process.env.MONGO_URL, {
 }).then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
+    // ONLY ADD ONE TIME!
     // User.insertMany(users);
     // Post.insertMany(posts);
 }).catch((error) => console.log(`${error} did not connect`));
